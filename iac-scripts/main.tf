3076bfd8125657a533b143dd990d61604b32b7cb
n509249 <divya.basavanagouda@ba.com>
2022-12-15 10:19:59 +0000

    updated yml

--- iac-scripts/main.tf
+++ iac-scripts/main.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {}
 }
 resource "aws_ssm_parameter" "foo" {
-  name  = "foo"
+  name  = "foo1"
   type  = "String"
   value = "barr"
 }
--- main.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "s3" {}
-}
-resource "aws_ssm_parameter" "foo" {
-  name  = "foo1"
-  type  = "String"
-  value = "barr"
-}
